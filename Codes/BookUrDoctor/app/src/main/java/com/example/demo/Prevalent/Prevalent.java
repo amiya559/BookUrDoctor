b5e490271a8dd1816f4d328e1e2baf0ce3f701e8
unknown <arrout29@gmail.com>
2019-11-12 22:48:04 +0530

    Updated

---
+++ Codes/BookUrDoctor/app/src/main/java/com/example/demo/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.demo.Prevalent;
+
+import com.example.demo.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
